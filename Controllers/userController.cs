c6fe9ea65d60a94ea039233cdc24dc6674d5e116
CallenThomason <thomasoncallen@gmail.com>
2026-09-21 10:27:34 -0700

    Inntial Commit

---
+++ Controllers/userController.cs
@@ -0,0 +1,45 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniChallenge11_APIBeginningsCThomason.Controllers
+{
+
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class userController : ControllerBase
+    {
+        
+    [HttpGet]
+    [Route("sayhello/{name}")]
+    public string GreetUser(string name)
+    {
+        return $"Hello, {name}"; 
+    }//end of method GreetUser
+    [HttpGet]
+    [Route("addtwonumnbers/{num1}/{num2}")]
+    public string addTwoNumbers(string num1, string num2)
+        {
+            
+            bool isNum = int.TryParse(num1, out int newNum);
+            bool isNum2 = int.TryParse(num2, out int newNum2);
+             if (!isNum || !isNum2)
+           {
+               return "Erorr: User did not input a number"; 
+            }
+               int sum =  newNum + newNum2; 
+            
+            return $"The sum of {newNum} and {newNum2} is {sum}"; 
+        }//end of method addTwoNumbers
+
+        [HttpGet]
+        [Route("wakeup/{name}/{time}")]
+
+        public string wakeUp(string name, string time)
+        {
+            return $"{name} woke up at {time}"; 
+        }
+
+    }//end of class
+}
