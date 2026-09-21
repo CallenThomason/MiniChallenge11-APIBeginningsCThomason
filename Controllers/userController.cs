76249cd0886e80f1c869d27efcd2757b21e1065e
CallenThomason <thomasoncallen@gmail.com>
2026-09-21 14:42:08 -0700

    Included Action Result, 404, and 200 results. Also add mini project 4-5

--- Controllers/userController.cs
+++ Controllers/userController.cs
@@ -1,4 +1,5 @@
 
+using System.Reflection.Metadata.Ecma335;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MiniChallenge11_APIBeginningsCThomason.Controllers
@@ -13,33 +14,66 @@ namespace MiniChallenge11_APIBeginningsCThomason.Controllers
         
     [HttpGet]
     [Route("sayhello/{name}")]
-    public string GreetUser(string name)
+    public ActionResult<string> GreetUser(string name)
     {
-        return $"Hello, {name}"; 
+        return Ok($"Hello, {name}"); 
     }//end of method GreetUser
     [HttpGet]
     [Route("addtwonumbers/{num1}/{num2}")]
-    public string addTwoNumbers(string num1, string num2)
+    public ActionResult<string> addTwoNumbers(string num1, string num2)
         {
             
             bool isNum = int.TryParse(num1, out int newNum);
             bool isNum2 = int.TryParse(num2, out int newNum2);
              if (!isNum || !isNum2)
            {
-               return "Erorr: User did not input a number"; 
+               return NotFound("One of your numbers contained invalid characters. Input a number and try again. "); 
             }
                int sum =  newNum + newNum2; 
             
-            return $"The sum of {newNum} and {newNum2} is {sum}"; 
+            return Ok($"The sum of {newNum} and {newNum2} is {sum}"); 
         }//end of method addTwoNumbers
 
         [HttpGet]
         [Route("wakeup/{name}/{time}")]
 
-        public string wakeUp(string name, string time)
+        public ActionResult<string> wakeUp(string name, string time)
         {
-            return $"{name} woke up at {time}"; 
-        }
+            return Ok($"{name} woke up at {time}"; 
+        } //end of method wakeUp
 
+        [HttpGet("comparison/{num1}/{num2}")]
+
+        public ActionResult<string> comparison(string num1, string num2)
+        {
+            bool isNum = int.TryParse(num1, out int newNum1); 
+            bool isNum2 = int.TryParse(num2, out int newNum2); 
+            if(isNum && isNum2)
+            {
+                if(newNum1 == newNum2)
+                {
+                    return Ok($"{newNum1} is equal too {newNum2}, and {newNum2} is equal too {newNum1}");
+                }
+                else if(newNum1 > newNum2)
+                {
+                    return Ok($"{newNum1} is greater than {newNum2}, and {newNum2} is less than {newNum1}");
+                }
+                else
+                {
+                    return Ok($"{newNum1} is less than {newNum2}, and {newNum2} is greater than {newNum1}");
+                }
+            }//end if
+            return NotFound("One of the values you input is not valid. Input a number.");
+        }//end of method comparison
+
+        [HttpGet("madlib/{name}/{animal}/{animalName}/{num}/{noun}/{color}/{verb}/{place}/{adj}/{food}")]
+        
+        //TEST: http://localhost:5007/api/user/madlib/Callen/Horse/Tyler/2/sword/red/run/tacobell/hairy/beans
+        public ActionResult<string> madLib(string name, string animal, string animalName, string num, string noun, string color, string verb, string place, string adj, string food)
+        {
+            
+            return Ok($"There once was a man named {name}. He had a trusty pet {animal} named {animalName}. {name} and {animalName} did everything together. When {animalName} turned {num}, {name} gave him a {color} {noun}. Then the two {verb} to {place}. They saw a {adj} car, and ate all you could eat {food}! THE END! ");
+        }
+       
     }//end of class
 }
